6aab0497713fdf978620b47ab9f8c8f25cf5819d
moses <mnagulama@gmail.com>
2021-10-05 00:45:53 +0300

    Changed the HTML for the site.

--- schools/urls.py
+++ schools/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('Elearning/', views.Elearning, name='Elearning'),
+    path('Seniorone/', views.Seniorone, name='Seniorone'),
+    path('Seniortwo/', views.Seniortwo, name='Seniortwo'),
+    path('Seniorthree/', views.Seniorthree, name='Seniorthree'),
+    path('Seniorfour/', views.Seniorfour, name='Seniorfour'),
+    path('Seniorfive/', views.Seniorfive, name='Seniorfive'),
+    path('Seniorsix/', views.Seniorsix, name='Seniorsix'),
 ]
 
--- schools/views.py
+++ schools/views.py
@@ -6,6 +6,17 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'schools/post_list.html', {'posts':posts})
-
 def Elearning(request):
     return render(request,'schools/Elearning.html')
+def Seniorone(request):
+    return render(request, 'schools/Seniorone.html')
+def Seniortwo(request):
+    return render(request, 'schools/Seniortwo.html')
+def Seniorthree(request):
+    return render(request, 'schools/Seniorthree.html')
+def Seniorfour(request):
+    return render(request, 'schools/Seniorfour.html')
+def Seniorfive(request):
+    return render(request, 'schools/Seniorfive.html')
+def Seniorsix(request):
+    return render(request, 'schools/Seniorsix.html')
