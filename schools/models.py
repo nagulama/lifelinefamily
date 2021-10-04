8ccd91d301c18d888063c1ad38edbcf5da4c980c
moses <mnagulama@gmail.com>
2021-10-04 20:24:19 +0300

    Changed the HTML for the site.

--- schools/models.py
+++ schools/models.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 class Post(models.Model):
     auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    cover = models.ImageField(upload_to='images/', null=True, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
